b575e2685f0026821cf536e66ec93d8612892b0d
ice <ice@ice-PC>
2017-02-07 18:13:48 +0800

    Signed-off-by: ice <ice@ice-PC>

--- js/p.js
+++ js/p.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	
+	$(".thumb").bind("click", function(event) {
+		var conId = $(this).attr("id") + "Con";
+		$(".imgDiv>div").hide();
+		$("#" + conId).show();
+		
+		$(".thumb").removeClass("active");
+		$(this).addClass("active");
+	});
 });
